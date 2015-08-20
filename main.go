0a2babab79e17d3f99b3c1cd24927a441a657132
shaalx <1316011195@qq.com>
2015-08-20 12:52:46 +0800

    "hello"

--- main.go
+++ main.go
@@ -1,25 +1,52 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/everfore/exc"
 	"os"
 )
+
+var (
+	quit   = false
+	commit = ""
+)
+
+func init() {
+	flag.BoolVar(&quit, "q", false, "-q: quit add all")
+	flag.StringVar(&commit, "m", "", "-m: commit content")
+}
+
+func main() {
+	flag.Parse()
+	fmt.Println(quit)
+	first := "git add -A"
+	if quit {
+		first = "git status"
+	}
+	git := exc.NewCMD(first).Wd()
+	_, err := git.Debug().Do()
+	if checkerr(err) {
+		os.Exit(-1)
+	}
+	fmt.Println(commit)
+	if len(commit) > 0 {
+		commit = fmt.Sprintf(`git commit -m "%s"`, commit)
+	} else {
+		commit = `git commit -m "auto"`
+	}
+	_, err = git.Reset(commit).Do()
+	if checkerr(err) {
+		os.Exit(-1)
+	}
+	git.Reset("git status").Execute()
+	return
+	git.Reset("git push origin master").Execute()
+}
 
 func checkerr(err error) bool {
 	if err != nil {
 		return true
 	}
 	return false
-}
-func main() {
-	git := exc.NewCMD("git add -A").Wd()
-	_, err := git.Debug().Do()
-	if checkerr(err) {
-		os.Exit(-1)
-	}
-	_, err = git.Reset(`git commit -m "auto"`).Do()
-	if checkerr(err) {
-		os.Exit(-1)
-	}
-	git.Reset("git push origin master").Execute()
 }
