aa4999fe371e23744f7998ed5a98129a7989897a
shaalx <shaalx@yahoo.com.sg>
2016-06-02 23:51:27 +0800

    auto

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func main() {
 		if len(tag) > 0 {
 			commit = fmt.Sprintf("git commit -m %s", tag)
 		} else {
-			commit = `git commit -m auto [skip ci]`
+			commit = `git commit -m auto`
 		}
 	}
 	_ = git.Reset(commit).Execute()
