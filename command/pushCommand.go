09c84ce01fb4907410f1bae5e253fc36bde9a27c
shiyongbin <shiyongbin@ezbuy.com>
2017-12-30 00:31:45 +0800

    psh

---
+++ command/pushCommand.go
@@ -0,0 +1,146 @@
+package command
+
+import (
+	"fmt"
+
+	"github.com/everfore/exc"
+	// cr "github.com/fatih/color"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var Command = &cobra.Command{
+	Use:   "push",
+	Short: "git push",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		size := len(args)
+		if size > 0 {
+			viper.Set("commit", args[0])
+		} else {
+			viper.Set("commit", "the same as the last")
+		}
+		Excute()
+	},
+}
+
+type Repo struct {
+	Commition    string
+	Branch       string
+	RemoteBranch string
+	Remote       string
+	Tag          string
+	git          *exc.CMD
+}
+
+func init() {
+	Command.PersistentFlags().BoolP("quit", "q", false, "quit add all, just push the committed code \n\tgit push")
+	Command.PersistentFlags().BoolP("force", "F", false, "-f: push --force")
+	Command.PersistentFlags().BoolP("only_commit", "o", false, "-o only commit")
+	Command.PersistentFlags().StringP("commit", "m", "", "-m $commit, commit and push \n\tgit add -A;git commit $commit;git push")
+	Command.PersistentFlags().StringP("remote", "R", "origin", "-r $remote \n\tgit push $origin")
+	Command.PersistentFlags().StringP("branch", "b", "", "-b $branch \n\tgit push $origin $branch:$remote_branch")
+	Command.PersistentFlags().StringP("remote_branch", "r", "", "-rb $remote_branch \n\tgit push $origin $branch:$remote_branch")
+	Command.PersistentFlags().StringP("tag", "t", "", "-t $tag \n\tgit tag -a $tag -m $tag;git push $origin --tags $tag:$tag")
+	Command.PersistentFlags().BoolP("ignore_dlt_tag", "d", false, "-d: delete the tag after 50 seconds \n\tgit tag -d $tag;git push $origin --tags :$tag")
+
+	viper.BindPFlag("quit", Command.PersistentFlags().Lookup("quit"))
+	viper.BindPFlag("force", Command.PersistentFlags().Lookup("force"))
+	viper.BindPFlag("force", Command.PersistentFlags().Lookup("force"))
+	viper.BindPFlag("only_commit", Command.PersistentFlags().Lookup("only_commit"))
+	viper.BindPFlag("commit", Command.PersistentFlags().Lookup("commit"))
+	viper.BindPFlag("remote", Command.PersistentFlags().Lookup("remote"))
+	viper.BindPFlag("branch", Command.PersistentFlags().Lookup("branch"))
+	viper.BindPFlag("remote_branch", Command.PersistentFlags().Lookup("remote_branch"))
+	viper.BindPFlag("tag", Command.PersistentFlags().Lookup("tag"))
+	viper.BindPFlag("ignore_dlt_tag", Command.PersistentFlags().Lookup("ignore_dlt_tag"))
+}
+
+func Excute() error {
+	var repo Repo
+	repo.Init()
+
+	return repo.ExcuteGit()
+}
+
+func LocalBranch() string {
+	bs, err := exc.NewCMD("git rev-parse --abbrev-ref HEAD").DoNoTime()
+	if err != nil {
+		panic(err)
+	}
+	cb := string(bs[:len(bs)-1])
+	fmt.Printf("* %s\n", cb)
+	return cb
+}
+
+func (r *Repo) Init() {
+	r.git = exc.NewCMD("").Debug()
+
+	r.Commition = viper.GetString("commit")
+	branch := LocalBranch()
+
+	vb := viper.GetString("branch")
+	if vb != "" {
+		r.Branch = vb
+	} else {
+		r.Branch = branch
+	}
+
+	vrb := viper.GetString("remote_branch")
+	if vrb != "" {
+		r.RemoteBranch = vrb
+	} else {
+		r.RemoteBranch = branch
+	}
+
+	vr := viper.GetString("remote")
+	if vr != "" {
+		r.Remote = vr
+	} else {
+		r.Remote = "origin"
+	}
+}
+
+func (r *Repo) ExcuteGit() error {
+	r.Commit()
+	r.Push()
+	return nil
+}
+
+func (r *Repo) Status() {
+	r.git.Reset(r.status()).Execute()
+}
+
+func (r *Repo) status() string {
+	return `git status`
+}
+
+func (r *Repo) Commit() {
+	r.Status()
+	if viper.GetBool("quit") {
+		return
+	}
+
+	r.git.Reset("git add -A").Execute()
+	r.git.Reset(r.commit()).Execute()
+	r.Status()
+}
+
+func (r *Repo) commit() string {
+	return fmt.Sprintf(`git commit -m %s`, r.Commition)
+}
+
+func (r *Repo) Push() {
+	if viper.GetBool("only_commit") {
+		return
+	}
+	r.git.Reset(r.push()).Execute()
+}
+
+func (r *Repo) push() string {
+	force := ""
+	if viper.GetBool("force") {
+		force = "-f "
+	}
+	return fmt.Sprintf("git push %s%s %s:%s", force, r.Remote, r.Branch, r.RemoteBranch)
+}
