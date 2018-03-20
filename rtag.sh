737e2b21ba4bf04a61541def74f1030233baae15
shiyongbin <shiyongbin@ezbuy.com>
2018-03-20 23:23:46 +0800

    auto

--- rtag.sh
+++ rtag.sh
@@ -4,9 +4,15 @@ git add .
 git commit -m "auto"
 echo "======="
 osascript <<EOF
-tell application "iTerm 2"
-	tell application "System Events" 
-         display dialog "bal bal" 
+tell application "iTerm"
+    activate
+    set t to (make new terminal)
+    tell t
+        tell (make new session at the end of sessions)
+            exec command "cd Downloads"
+            exec command "clear"
+            exec command "pwd"
+        end tell
     end tell
 end tell
 EOF
