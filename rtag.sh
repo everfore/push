e3a997d3fbd0086d84e5f5c458a76996462195b9
shiyongbin <shiyongbin@ezbuy.com>
2018-03-21 00:16:20 +0800

    auto

--- rtag.sh
+++ rtag.sh
@@ -12,7 +12,7 @@ tell application "System Events"
         set frontmost to true
         keystroke "t" using {command down}
         keystroke "baidu.com"
-        key down Enter
+        key down return
 	end tell
  #    tell process "iTerm2"
  #        set frontmost to true
