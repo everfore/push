725470c73973b23488e45b34ca41ed105f6b278d
shiyongbin <shiyongbin@ezbuy.com>
2018-03-23 22:19:58 +0800

    the+same+as+the+last

--- rtag.sh
+++ rtag.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 osascript <<EOF
-# tell application "iTerm2"
-# tell application "System Events"
-#     keystroke "git rebase -i HEAD~2"
-#     keystroke return
-# end tell
-# end tell
+tell application "iTerm2"
+tell application "System Events"
+    keystroke "git rebase -i HEAD~2"
+    keystroke return
+end tell
+end tell
 
 # # tell application "System Events"
 # 	set t to application "iTerm2"
